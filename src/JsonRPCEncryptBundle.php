0a80d9fd952323d3a84c46617b576f5766e7b45e
github-ci <you@example.com>
2025-05-11 08:52:25 +0000

    fix

--- src/JsonRPCEncryptBundle.php
+++ src/JsonRPCEncryptBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\JsonRPCEncryptBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class JsonRPCEncryptBundle extends Bundle
+class JsonRPCEncryptBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Tourze\JsonRPCCallerBundle\JsonRPCCallerBundle::class => ['all' => true],
+        ];
+    }
 }
